999f994debbe8bde197bf41ff7754e7e0647fba6
Arseni Konakhau <arseni.konakhau@gmail.com>
2025-08-31 17:31:06 +0300

    init sb project

---
+++ src/main/java/com/mcpforeverybody/deepsekport/DeepsekportApplication.java
@@ -0,0 +1,13 @@
+package com.mcpforeverybody.deepsekport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepsekportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepsekportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcpforeverybody/deepsekport/DeepsekportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcpforeverybody.deepsekport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepsekportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
